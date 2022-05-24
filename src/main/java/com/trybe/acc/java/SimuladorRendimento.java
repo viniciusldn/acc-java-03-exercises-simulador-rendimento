403e4ee51f3e75da22ac8eced73547b9ab87b2b6
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-05-24 11:29:14 -0300

    feat: metodo simularRendimentoAnos implementado

--- src/main/java/com/trybe/acc/java/SimuladorRendimento.java
+++ src/main/java/com/trybe/acc/java/SimuladorRendimento.java
@@ -1,9 +1,46 @@
 package com.trybe.acc.java;
 
+import java.time.LocalDateTime;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class SimuladorRendimento.
+ */
 public class SimuladorRendimento {
 
-  public int simuladorDeRendimentoAnos(double investimento, double objetivo) {
-    return 0;
+  /** The data atual. */
+  private LocalDateTime dataAtual = LocalDateTime.now();
+
+  /** The ano atual. */
+  private int anoAtual = dataAtual.getYear();
+
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    SimuladorRendimento obj = new SimuladorRendimento();
+    double inicial = 500;
+    double objetivo = 5000;
+    System.out.println("Com " + inicial + " seu rendimento será " + objetivo + " no ano "
+        + obj.simularRendimentoAnos(inicial, objetivo));
+  }
+
+  /**
+   * Simular rendimento anos.
+   *
+   * @param investimento the investimento
+   * @param objetivo     the objetivo
+   * @return the int
+   */
+  public int simularRendimentoAnos(double investimento, double objetivo) {
+    int anoObjetivo = this.anoAtual;
+    while (investimento <= objetivo) {
+      investimento *= 1.1;
+      anoObjetivo++;
+    }
+    return anoObjetivo;
   }
 
 }
